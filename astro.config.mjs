9c6671f64cf0973edd9253582c57bcebebe524da
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-15 16:41:06 +0200

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'static'
+  integrations: [preact(), tailwind()],
+  server: { port: 4321 },
+  output: 'static',
 });
